c4c9e1bea7cc7519a7313207244980565aea1f07
loki-global <lohithg6006@gmail.com>
2026-01-24 01:29:42 +0530

    Add landing page and cards dashboard UI

--- mainrun.py
+++ mainrun.py
@@ -1,15 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from models import db
 from routecards import card_routes # projects routes file or blueprint
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./selections.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'selections.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app) 
 
 app.register_blueprint(card_routes) # Register routes blueprint
+
+# Landing page route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Cards dashboard route
+@app.route('/cards/')
+def cards():
+    return render_template('cards.html')
 
 with app.app_context():
     db.create_all()
